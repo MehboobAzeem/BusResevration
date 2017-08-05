d553b39f00a6bf3638e078d1f9b8ab3a5cbf99cd
MehboobAzeem <30482100+MehboobAzeem@users.noreply.github.com>
2017-08-06 02:34:04 +0500

    BusResevrion
    My First Project

---
+++ Controllers/BusresevrationController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BusResevration.Models;
+
+namespace BusResevration.Controllers
+{
+    public class BusresevrationController : ApiController
+    {
+        public bool NewUser(Resevration U)
+        {
+
+            return true;
+        }
+
+        public bool Resevration(Resevration R)
+        {
+
+            return true;
+        }
+       
+        public string CheckReservtionsStatus(int ticketNumber)
+        {
+            Resevration r = new Resevration();
+            return r.CurrentStatus;
+        }
+        public bool CancelResevrtion(int ResevrtionId)
+        {
+
+            return true;
+        }
+
+        public List<string>getResevrtions()
+        {
+            List<string> resevrtions = new List<string>();
+            return resevrtions;
+        } 
+
+
+    }
+}
---
+++ Models/Resevration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BusResevration.Models
+{
+    public class Resevration : ApiController
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int TicketNumber { get; set; }
+        public string PickPlace { get; set; }
+        public string DropPlace { get; set; }
+        public string CurrentStatus { get; set; }
+    }
+}
